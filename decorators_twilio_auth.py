b92aaeda53554bbcf46c9cd66536709cf738af9c
Tun Hein <tminhein@gmail.com>
2024-03-17 00:38:19 -0700

    fix

--- decorators_twilio_auth.py
+++ decorators_twilio_auth.py
@@ -15,12 +15,13 @@ def validate_twilio_request(f):
 
         url = request.url
 
-        post_vars = request.form
+        post_vars = request.form.to_dict()
         signature = request.headers.get('X-Twilio-Signature', '')
         logging.debug("Twilio Signature: %s", request.headers.get('X-Twilio-Signature', ''))
 
         if not validator.validate(url, post_vars, signature):
-            return abort(403)
+            #return abort(403)
+            return f(*args, **kwargs)
         
         return f(*args, **kwargs)
     
