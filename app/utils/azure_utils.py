ee36606bc1fd0b027b5ba8031581a372c3c48066
Tun Hein <tminhein@gmail.com>
2024-04-17 14:36:11 -0700

    fix azure credential

--- app/utils/azure_utils.py
+++ app/utils/azure_utils.py
@@ -1,4 +1,4 @@
-from azure.identity import DefaultAzureCredential
+from azure.identity import EnvironmentCredential, ClientSecretCredential, AzureCliCredential
 from azure.keyvault.secrets import SecretClient
 import os
 
@@ -6,7 +6,12 @@ def get_google_cloud_service_account_from_key_vault():
     key_vault_name = os.environ.get('KEY_VAULT_NAME')
     key_vault_uri = f"https://{key_vault_name}.vault.azure.net/"
 
-    credential = DefaultAzureCredential()
+    azure_client_id = os.environ.get('AZURE_CLIENT_ID')
+    azure_client_secret = os.environ.get('AZURE_CLIENT_SECRET')
+    azure_tenant_id = os.environ.get('AZURE_TENANT_ID')
+
+    credential = ClientSecretCredential(tenant_id=azure_tenant_id, client_id=azure_client_id, client_secret=azure_client_secret)
+
     client = SecretClient(vault_url=key_vault_uri, credential=credential)
 
     secret_name = "google-service-account"
