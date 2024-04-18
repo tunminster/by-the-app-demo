1e2b192281e42ef848163c8eb11330364f83f05a
Tun Hein <tminhein@gmail.com>
2024-04-18 16:30:12 -0700

    fix

--- app/utils/speech_services.py
+++ app/utils/speech_services.py
@@ -5,6 +5,7 @@ from app.utils.azure_utils import get_google_cloud_service_account_from_key_vaul
 from google.oauth2.service_account import Credentials
 import os
 import json
+import re
 
 def get_credentials():
     service_account_info = json.loads(get_google_cloud_service_account_from_key_vault())
@@ -30,15 +31,19 @@ def synthesize_speech(text, language_code="en-US", voice_name="en-US-Wavenet-D")
     blob_service_client = BlobServiceClient.from_connection_string(os.getenv('BYTHEAPP_AZURE_STORAGE_CONNECTION_STRING'))
     container_name = current_app.config['AZURE_STORAGE_VOICE_CONTAINER']
 
+    sanitized_text = re.sub(r'[^a-zA-Z0-9]', '', text[:10])
+
     # Generate a unique filename for the audio file
     # Ensure that the filename is URL-safe and unique
-    blob_name = f"{text[:10].replace(' ', '')}-{os.urandom(4).hex()}.mp3"
+    blob_name = f"{sanitized_text}-{os.urandom(4).hex()}.mp3"
     
     # Get the blob client for uploading the file
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
 
     # Upload the audio content to Azure Blob Storage
     blob_client.upload_blob(response.audio_content, overwrite=True)
+
+   
 
     # Assuming the container is configured to allow public access
     audio_url = f"https://{blob_service_client.account_name}.blob.core.windows.net/{container_name}/{blob_name}"
