0084c9f8d70a50bb8b8321ead20cbef35b61ea63
Tun Hein <tminhein@gmail.com>
2024-07-31 17:33:15 -0700

    fix

--- app/routes/voice.py
+++ app/routes/voice.py
@@ -52,7 +52,7 @@ def voice():
 
     greeting = "Welcome to ABC Bank, how can we assist you today?"
 
-    speech_file_path = Path(__file__).parents[1] / "static" / "speech.mp3"
+    speech_file_path = Path(__file__).resolve().parent.parent / "static" / "speech.mp3"
     print("speech_file_path xxxx ", speech_file_path)
     response = openai.audio.speech.create(
         model="tts-1",
