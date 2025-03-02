7134722cc9404b3987e2b6e992a0e2f566022922
Tun Hein <tminhein@gmail.com>
2025-03-02 12:10:29 -0800

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,11 @@ import os
 from fastapi.routing import APIRouter
 from contextlib import asynccontextmanager
 
+# Import routers and register them
+from app.routes.voice import voice_router
+from app.routes.register import register_router
+from app.routes.train_data import train_data_router
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,22 +23,19 @@ async def lifespan(app: FastAPI):
     
     # Cleanup actions (if necessary)
 
-app = FastAPI(lifespan=lifespan)
+async def create_app():
+    app = FastAPI(lifespan=lifespan)
 
     # Load config from config.py directly
-app.state.config = {
-    "AZURE_STORAGE_CONTAINER":'bytheapp-training-data',
-    "TRAINING_BLOB_DATA_FILE":'training_data.csv',
-    "AZURE_STORAGE_VOICE_CONTAINER":'bytheapp-voice-data'
-}
+    app.state.config = {
+        "AZURE_STORAGE_CONTAINER":'bytheapp-training-data',
+        "TRAINING_BLOB_DATA_FILE":'training_data.csv',
+        "AZURE_STORAGE_VOICE_CONTAINER":'bytheapp-voice-data'
+    }
 
+    app.include_router(voice_router, prefix="/voice", tags=["voice"])
+    app.include_router(register_router, prefix="/register", tags=["register"])
+    app.include_router(train_data_router, prefix="/train_data", tags=["train_data"])
 
+    return app
 
-# Import routers and register them
-from app.routes.voice import voice_router
-from app.routes.register import register_router
-from app.routes.train_data import train_data_router
-
-app.include_router(voice_router, prefix="/voice", tags=["voice"])
-app.include_router(register_router, prefix="/register", tags=["register"])
-app.include_router(train_data_router, prefix="/train_data", tags=["train_data"])
