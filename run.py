d741a6a82171ebf039bed294ad73ecf1ddabf331
Tun Hein <tminhein@gmail.com>
2025-03-01 20:15:19 -0800

    fix

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from app import create_app  # Import the function, not the app instance
+from fastapi_cache2 import FastAPICache
+from fastapi_cache2.backends.inmemory import InMemoryBackend
+
 import uvicorn
 
 app = create_app()  # Call the function to create the FastAPI app
