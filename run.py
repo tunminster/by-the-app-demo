77cf961c793e1bf06f7f469c5b4399e30cb4e44e
Tun Hein <tminhein@gmail.com>
2025-03-02 12:37:22 -0800

    fix

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import uvicorn
 async def main():
     app = await create_app()  # Await here
 
-    uvicorn.run(app, host="0.0.0.0", port=80, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=80, reload=True, factory=True)
 
 if __name__ == "__main__":
     asyncio.run(main())  # Run the async function
